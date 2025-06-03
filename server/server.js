dbcb9e15bd0873ae8d5c7b56963e0705d3be8f76
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-06-02 22:37:11 -0700

    I think I almost have the deploy issues solved

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ server.use(express.json());
 
 server.use(cors({
     // Frontend server
-    origin: 'http://localhost:5173', 
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173', 
     credentials: true
   }));
 
@@ -29,9 +29,9 @@ server.use('/pdf', pdfRouter);
 // Serve the static html file when not testing
 if (process.env.NODE_ENV !== 'test') {
     server.use(express.static(path.join(__dirname, '../client/dist')));
-    // server.get('*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    // });
+    server.get('/', (req, res) => {
+        res.send('Backend is working')
+    });
 }
 
 
